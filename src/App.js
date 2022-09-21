def283dba3cba3bcced2393c946aa9152464cd9b
Vlad <belousov7vlad@gmail.com>
2022-09-21 21:41:20 +0300

    header desc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import AboutReabilitation from "./components/AboutReabilitation/AboutReabilitation";
 import Slider from "./components/Slider/ Slider";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <Slider/>
             <AboutReabilitation/>
+            <Footer />
         </div>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,27 @@
-const Header = () => {
-    
-    return <header>
+import HeaderLogo from "../../images/logo_header.svg"
 
+const Header = () => {
+
+    return <header className="header">
+
+        <div className="header__content">
+
+            <img className="header__logo" src={HeaderLogo} alt="Логотип" />
+
+            <div className="header__menu">
+
+                <ul className="links">
+
+                    <li className="links__item"><a className="link" href="ww">Получить помощь</a></li>
+                    <li className="links__item"><a className="link" href="ww">О реабилитации</a></li>
+                    <li className="links__item"><a className="link" href="ww">Контакты</a></li>
+                </ul>
+
+
+                <button className="header__button">ПОМОЧЬ ПРОЕКТУ</button>
+
+            </div>
+        </div>
     </header>;
 };
 
