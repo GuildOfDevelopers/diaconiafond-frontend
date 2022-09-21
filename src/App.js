98b776a074b08f8aad23caee8f85c654ceee0211
Nikita <nikitaleonov00@mail.ru>
2022-09-21 17:42:34 +0300

    added slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AboutReabilitation from "./components/AboutReabilitation/AboutReabilitation";
+import Slider from "./components/Slider/ Slider";
 
 function App() {
-  return (
-    <div className="App">
-      <AboutReabilitation />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Slider/>
+            <AboutReabilitation/>
+        </div>
+    );
 }
 
 export default App;
