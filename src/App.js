3dd5515ee915a2a7f4a7797b43079e3397c20b24
Полина Каминская <polinakaminskaa@MacBook.local>
2022-09-22 19:13:28 +0300

    you can help desktop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import AboutReabilitation from "./components/AboutReabilitation/AboutReabilitation";
-import Slider from "./components/Slider/ Slider";
+// import AboutReabilitation from "./components/AboutReabilitation/AboutReabilitation";
+// import Slider from "./components/Slider/ Slider";
+import YouCanHelp from "./components/YouCanHelp/YouCanHelp";
 
 function App() {
     return (
         <div className="App">
-            <Slider/>
-            <AboutReabilitation/>
+            {/* <Slider/> */}
+            {/* <AboutReabilitation/> */}
+            <YouCanHelp/>
         </div>
     );
 }
