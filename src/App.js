462e493245548f38b14988c70006a380a8f9f659
Ruslan Gaziev <grinrus@yandex.ru>
2022-09-25 14:59:24 +0300

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import AboutReabilitation from "./components/AboutReabilitation/AboutReabilitation";
-import Slider from "./components/Slider/ Slider";
+import Slider from "./components/Slider/Slider";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import ImportanceReabilitaion from "./components/ImportanceReabilitaion/ImportanceReabilitaion";
