7f739fc3870ec1460e73fa218ffce5c11b6f6049
Nikita <nikitaleonov00@mail.ru>
2022-09-25 12:29:43 +0300

    fixed slider in mozilla

--- src/components/Slider/SlideList.jsx
+++ src/components/Slider/SlideList.jsx
@@ -1,20 +1,25 @@
 import React, {useContext} from "react";
 import Slide from "./Slide";
 import {SliderContext} from "./ Slider";
+import {browserName} from 'react-device-detect';
 
 import style from "./Slider.module.scss";
 
 export default function SlidesList() {
   const {slideNumber, items} = useContext(SliderContext);
 
+
   return (
     <div
       className={style.slide_list}
       style={{transform: `translateX(-${slideNumber * 100}%)`}}
     >
-      {items.map((slide, index) => (
+
+      {browserName !== 'Firefox' && items.map((slide, index) => (
         <Slide key={index} data={slide}/>
       ))}
+
+      {browserName === 'Firefox' && <Slide key={items[0].index} data={items[0]}/>}
     </div>
   )
 }
